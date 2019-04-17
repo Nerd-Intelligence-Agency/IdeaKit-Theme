fa3c4b785f4ecd4cf36e198e2eaf2cd630063cf6
Bryant Cole <bcol120@gmail.com>
2019-04-17 14:17:32 -0400

    Add function.php, screenshot, style.css

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @author Nerd Intelligence Agency
+* @copyright 2019
+**/
+
+if (!defined('ABSPATH')) die();
+
+//enque divi theme
+function ideakit_ct_enqueue_parent() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
