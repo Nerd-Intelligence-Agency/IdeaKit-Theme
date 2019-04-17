4407b6e50de0b1df7f3344a636abdf8b494848fe
Bryant Cole <bcol120@gmail.com>
2019-04-17 15:54:13 -0400

    update plugin updater remove theme.json

--- functions.php
+++ functions.php
@@ -11,3 +11,17 @@ function ideakit_ct_enqueue_parent() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'ideakit_ct_enqueue_parent' );
+
+//Check for theme updates
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/Nerd-Intelligence-Agency/IdeaKit-Theme',
+  __FILE__,
+  'IdeaKit-Theme'
+);
+
+//Optional: If you're using a private repository, specify the access token like this:
+//$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
