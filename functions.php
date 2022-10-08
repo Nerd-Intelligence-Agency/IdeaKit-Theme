3432f7d028cb0bb065a7bd53e179b5ad5302c41a
bcol120 <bryant.cole@nerdintel.com>
2022-10-08 19:10:13 -0400

    intial

--- functions.php
+++ functions.php
@@ -7,8 +7,21 @@
 if (!defined('ABSPATH')) die();
 
 //enque divi theme
-function ideakit_ct_enqueue_parent() {
+function storekit_ct_enqueue_parent() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'ideakit_ct_enqueue_parent' );
 
+//Check for theme updates
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/Nerd-Intelligence-Agency/IdeaKit-Theme',
+  __FILE__,
+  'ideakit-theme'
+);
+
+//Optional: If you're using a private repository, specify the access token like this:
+//$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('master');
